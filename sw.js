954a4786df96b7a7bc10a00d9d3864f5b95daf5c
pedrochagasmaster <pedro.chagas@mastercard.com>
2026-09-26 13:30:23 +0000

    P4b: converge saved-session summary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "repforge-v315";
+const CACHE = "repforge-v316";
 const ASSETS = [
   "./", "./index.html", "./styles.css", "./motion-polish.css", "./manifest.webmanifest",
   "./vendor/motion/motion.js", "./vendor/dnd-kit/dnd-kit.js", "./vendor/dnd-kit/dnd-kit.runtime.js", "./motion-layer.js", "./motion-layer.js?v=305",
